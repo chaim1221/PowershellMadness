18da95ea817af546dc8b921aabb5b7ff56820979
Chaim Eliyah <chaimeliyah@gmail.com>
2015-10-21 16:15:42 -0700

    Machete Madness.

---
+++ MacheteMadness.ps1
@@ -0,0 +1,21 @@
+﻿param (
+    [int]$DaysAgo = 30,
+    [string]$FileType = "*.*"
+)
+
+[string]$webDirectory = "C:\inetpub\wwwroot"
+
+function DisplayUsage {
+    Write-Host ("In display usage")
+    Exit
+}
+
+try
+{
+    Get-ChildItem -filter $FileType -recurse -path C:\inetpub\wwwroot | where {$_.LastWriteTime -gt (get-date).AddDays(-1 * $DaysAgo)}
+}
+catch
+{
+    $Host.UI.WriteErrorLine("Whoops! An error occurred.")
+    DisplayUsage
+}
